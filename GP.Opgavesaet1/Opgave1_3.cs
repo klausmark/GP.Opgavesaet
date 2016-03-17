6ed27acc8de305901aeb0b83084eaa2f8afc7cf3
Klaus V. Mark (KLMA) <klma@danishcrown.dk>
2016-03-17 14:41:04 +0100

    Opgave 1.3 løst

--- GP.Opgavesaet1/Opgave1_3.cs
+++ GP.Opgavesaet1/Opgave1_3.cs
@@ -1,10 +1,19 @@
-﻿namespace GP.Opgavesaet1
+﻿using System;
+
+namespace GP.Opgavesaet1
 {
     public class Opgave1_3
     {
         public static void Run()
         {
+            Console.Write("Indtast antal aviser der skal omdeles:");
 
+            var antalAviser = uint.Parse(Console.ReadLine());
+
+            var bundter = antalAviser/50;
+            var rest = antalAviser%50;
+
+            Console.WriteLine($"Antal bundter:{bundter}, rest:{rest}");
         }
     }
 }
--- GP.Opgavesaet1/Program.cs
+++ GP.Opgavesaet1/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-//            Opgave1_0.Run();
-//            Opgave1_1.Run();
-            Opgave1_2.Run();
+            //Opgave1_0.Run();
+            //Opgave1_1.Run();
+            //Opgave1_2.Run();
             Opgave1_3.Run();
             Opgave1_4.Run();
             Opgave1_5.Run();
