9015b5b2488a9cdf1cc3e36c8d60569f76eaf3f6
Klaus V. Mark (KLMA) <klma@danishcrown.dk>
2016-03-17 14:51:03 +0100

    Opgave 1.4 lavet

--- GP.Opgavesaet1/Opgave1_4.cs
+++ GP.Opgavesaet1/Opgave1_4.cs
@@ -1,10 +1,19 @@
-﻿namespace GP.Opgavesaet1
+﻿using System;
+
+namespace GP.Opgavesaet1
 {
     public class Opgave1_4
     {
         public static void Run()
         {
-
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+            Console.WriteLine($"Output: {input.Length} (antal tegn)");
+            Console.WriteLine($"Output: {input.Replace('e', 'ø')} (e erstattet med ø)");
+            Console.WriteLine($"Output: {input.ToUpper()} (alle bogstaver store)");
+            var førsteTegnStort = input.ToLower();
+            førsteTegnStort = førsteTegnStort.Substring(0, 1).ToUpper() + førsteTegnStort.Substring(1);
+            Console.WriteLine($"Output: {førsteTegnStort} (første tegn stort)");
         }
     }
 }
